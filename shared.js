d4b7c5d378c04c3e3bb7b156baa161f45d7f3c62
Dmitrij Melichov <dmitrij.melichov@cognite.com>
2020-12-23 14:01:37 +0100

    Add listeners to close the modal

--- shared.js
+++ shared.js
@@ -1,10 +1,23 @@
 const backdrop = document.querySelector(".backdrop");
 const modal = document.querySelector(".modal");
 const selectPlanBtns = document.querySelectorAll(".plan button");
+const modalNoBtn = document.querySelector(".modal__action--negative");
 
-selectPlanBtns.forEach(element => {
+selectPlanBtns.forEach((element) => {
   element.addEventListener("click", () => {
     modal.style.display = "block";
     backdrop.style.display = "block";
-  })
+  });
 });
+
+const hideElement = (element) => {
+  element.style.display = "none";
+};
+
+const closeModal = () => {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+};
+
+backdrop.addEventListener("click", closeModal);
+modalNoBtn.addEventListener("click", closeModal);
