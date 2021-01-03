ca355c8282917c735b9278fb303c7d539a51aed6
Dmitrij Melichov <dmitrij.melichov@cognite.com>
2021-01-03 16:33:21 +0100

    Add transitions to modal/backdrop/sidenav

--- main.js
+++ main.js
@@ -6,13 +6,19 @@ const modalNoBtn = document.querySelector(".modal__action--negative");
 selectPlanBtns.forEach((element) => {
   element.addEventListener("click", () => {
     modal.classList.add("modal_open")
-    backdrop.classList.add("modal_open")
+    backdrop.style.display = "block";
+    setTimeout(() => {
+      backdrop.classList.add("modal_open")
+    }, 10);
   });
 });
 
 const closeModal = () => {
   modal.classList.remove("modal_open")
   backdrop.classList.remove("modal_open")
+  setTimeout(() => {
+    backdrop.style.display = "none";
+  }, 200); // this has to match the transition duration!
 };
 
 backdrop.addEventListener("click", closeModal);
--- shared.js
+++ shared.js
@@ -4,10 +4,16 @@ const sideDrawer = document.querySelector(".mobile-nav")
 
 sideDrawerBtn.addEventListener("click", () => {
   sideDrawer.classList.add("open")
-  backdrop.classList.add("open")
+  backdrop.style.display = "block";
+    setTimeout(() => {
+      backdrop.classList.add("open")
+    }, 10);
 });
 
 backdrop.addEventListener("click", () => {
   sideDrawer.classList.remove("open")
   backdrop.classList.remove("open")
+  setTimeout(() => {
+    backdrop.style.display = "none";
+  }, 200); // this has to match the transition duration!
 });
